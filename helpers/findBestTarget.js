1fad2c6ffba5a0c20346203f627cf55e3d7922e1
Shaska- <xshaska@gmail.com>
2024-07-07 21:55:50 +0200

    Added a helper to find the best target for hacking based on our current hacking level and max available money on the server

---
+++ helpers/findBestTarget.js
@@ -0,0 +1,30 @@
+import {crawl} from "/helpers/crawl.js";
+
+export async function findBestTarget(ns) {
+	let serverList = await crawl(ns);
+    let target = "null";
+    target = await BestTarget(ns, serverList);
+    return target;
+}
+
+async function BestTarget(ns, serverList){
+    let hackingLevel = ns.getHackingLevel();
+    let potentialTargets = []
+
+    for (var server in serverList){
+        var serverSecurity = ns.getServerRequiredHackingLevel(serverList[server]);
+        if (hackingLevel >= serverSecurity){
+            potentialTargets.push(serverList[server])
+            //ns.print(potentialTargets);
+        }
+    }
+
+    let bestTarget = "home"
+    for (var target in potentialTargets){
+        if (ns.getServerMaxMoney(potentialTargets[target]) > ns.getServerMaxMoney(bestTarget)){
+            bestTarget = potentialTargets[target]
+            ns.print("hello");
+        }
+    }
+    return bestTarget;
+}
