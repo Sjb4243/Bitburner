1815f02ee57b9a6ba10577a5580dfe7c566fed8a
Sam <sambennett4243@gmail.com>
2024-07-07 16:52:18 +0100

    Adding crawling for servers

---
+++ helpers/crawl.js
@@ -0,0 +1,23 @@
+/**
+ * [crawl crawls through the network and returns an array of servers]
+ * @return {[array]}      [array of all servers in the network (not player owned)]
+ */
+
+export async function crawl(ns){
+    let visited = [];
+    await recurse(ns, "home", visited)
+    return visited
+}
+
+async function recurse(ns, start, visited) {
+    if (!visited.includes(start)) {
+      visited.push(start);  
+    }
+    let servers = ns.scan(start);
+    for (let i = 0; i < servers.length; i++) {
+      let target = servers[i];
+      if (!visited.includes(target)) {
+        await recurse(ns, target, visited);
+      }
+    }
+  }
