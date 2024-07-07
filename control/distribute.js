a8cff53872d85dd1bf723351f4915534627ccc7c
Sam <sambennett4243@gmail.com>
2024-07-07 21:28:56 +0100

    Basic untested form of a distribution system

---
+++ control/distribute.js
@@ -0,0 +1,43 @@
+/** @param {NS} ns */
+/**
+ * [Cracks a server]
+ * @param  {[array]} arg1 [server array]
+ * @param  {[array]} arg2 [percentage array]
+ */
+
+export async function main(ns, servers, percentages) {
+    let totalRam = 0;
+    for (let i = 0; i < servers.length;i++){
+        if (!servers[i] != "home"){
+            totalRam = totalRam +  ns.getServerMaxRam()
+        }
+    }
+
+    let totalThreads = Math.floor(totalRam / 1.75) 
+
+    let growThreads = Math.floor(totalThreads * percentages[0])
+    let weakThreads = Math.floor(totalThreads * percentages[1])
+    let hackThreads = Math.floor(totalThreads * percentages[2])
+    let slicedArray = servers
+    while (growThreads > 0){
+        for (let i = 0; i < servers.length;i++){
+            if (servers[i] != "home"){
+            server = servers[i]
+            let usedThreads = Math.floor(ns.getServerMaxRam(server) - ns.getServerUsedRam(server) / 1.75)
+            if (growThreads - usedThreads < 0){
+                let over = Math.abs(growThreads - usedThreads)
+                usedThreads = usedThreads - over
+            }else{
+                //server expended
+                slicedArray.shift()
+            }
+            //CALL SCRIPT HERE WITH USED THREADS
+            growThreads = growThreads - usedThreads
+            }
+        }
+    }
+    //WEAKTHREADS
+    //HACKTHREADS
+    //BASICALLY THE SAME SO SHOUDL FUNCTIONISE IT
+}
+  
