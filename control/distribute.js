304e35988334198c509a83674bf2d9e072b2a674
Sam <105596361+Sjb4243@users.noreply.github.com>
2024-07-09 12:58:21 +0100

    Update distribute.js

--- control/distribute.js
+++ control/distribute.js
@@ -18,33 +18,33 @@ export async function main(ns, servers, percentages) {
         
     }
 
-    let totalThreads = Math.floor(totalRam / 1.75) 
-    const percentages = [0.4,0.4,0.2]
-    let growThreads = Math.floor(totalThreads * percentages[0])
-    let weakThreads = Math.floor(totalThreads * percentages[1])
-    let hackThreads = Math.floor(totalThreads * percentages[2])
-    let slicedArray = servers
-    let counter = 0
+    let totalThreads = Math.floor(totalRam / 1.75); 
+    const percentages = [0.4,0.4,0.2];
+    let growThreads = Math.floor(totalThreads * percentages[0]);
+    let weakThreads = Math.floor(totalThreads * percentages[1]);
+    let hackThreads = Math.floor(totalThreads * percentages[2]);
+    let slicedArray = servers;
+    let counter = 0;
     //While we still have threads left to allocate
     while (growThreads > 0){
         let server = slicedArray[counter];
         //get the amount of usable threads vs our ram cost
-        let usedThreads = Math.floor(ns.getServerMaxRam(server) - ns.getServerUsedRam(server) / 1.75)
+        let usedThreads = Math.floor(ns.getServerMaxRam(server) - ns.getServerUsedRam(server) / 1.75);
         //if the max threads use too much, get the amount we're over by and take it off of it
         //That way we can half use servers
         if (growThreads - usedThreads < 0){
-            let over = Math.abs(growThreads - usedThreads)
-            usedThreads = usedThreads - over
+            let over = Math.abs(growThreads - usedThreads);
+            usedThreads = usedThreads - over;
         //if not, this server has been expended so we slice it off of sliced array
         }else{
             //server expended
-            servers.shift()
+            servers.shift();
         }
         //Call script using usedthreads and then take it off of growthreads
         //CALL SCRIPT HERE WITH USED THREADS
-        ns.exec("http://grow.js", server, usedThreads, "n00dles")
+        await ns.exec("http://grow.js", server, usedThreads, "n00dles")
         ++counter;
-        growThreads = growThreads - usedThreads
+        growThreads = growThreads - usedThreads;
         }
     }
     //WEAKTHREADS
