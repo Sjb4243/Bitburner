32b17e867eaa87dc623c78f7154e226e71cd803b
Sam <sambennett4243@gmail.com>
2024-07-09 20:58:16 +0100

    Prevents infinite loops in distribute.js

--- control/distribute.js
+++ control/distribute.js
@@ -9,12 +9,9 @@
 export async function distribute(ns, servers, percentages, target) {
   let totalRam = 0;
 
-  for (let i = 0; i < servers.length; i++) {
-    totalRam = totalRam + ns.getServerMaxRam(servers[i])
-  }
 
   let totalThreads = 0;
-  for (var server in servers){
+  for (var server in servers) {
     totalThreads += Math.floor(ns.getServerMaxRam(servers[server]) / 1.75);
   }
   //let totalThreads = Math.floor(totalRam / 1.75);
@@ -26,19 +23,15 @@ export async function distribute(ns, servers, percentages, target) {
   servers = await start_distribute(ns, hackThreads, servers, "hack.js", target)
 }
 
-export async function start_distribute(ns, threads, serverlist, script, target){
+export async function start_distribute(ns, threads, serverlist, script, target) {
   let counter = 0
   const copyList = serverlist.slice();
-  while (threads > 0){
+  while (threads > 0 && counter < copyList.length) {
     let server = copyList[counter];
     let usedThreads = Math.floor((ns.getServerMaxRam(server) - ns.getServerUsedRam(server)) / 1.75);
-    if (usedThreads == 0) {
-      ++counter;
-      continue
-  } 
-  if (threads - usedThreads < 0) {
-     //if the max threads use too much, get the amount we're over by and take it off of it
-    //That way we can half use servers
+    if (threads - usedThreads < 0) {
+      //if the max threads use too much, get the amount we're over by and take it off of it
+      //That way we can half use servers
       let over = Math.abs(threads - usedThreads);
       usedThreads = usedThreads - over;
       //if not, this server has been expended so we slice it off of sliced array
@@ -46,7 +39,7 @@ export async function start_distribute(ns, threads, serverlist, script, target){
       //server expended
       serverlist.shift();
     }
-  await ns.exec(script, server, usedThreads, target);
+    await ns.exec(script, server, usedThreads, target);
     threads = threads - usedThreads;
     ++counter;
   }
